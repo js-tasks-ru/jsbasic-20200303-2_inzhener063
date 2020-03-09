60ef1285422c0817cc778dfe18f9b338d83fca69
Ruslan <inzhener063@mail.ru>
2020-03-09 19:36:50 +0400

    Added 1-module
    Решение задач №3,4,5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,13 @@
  * @param {string} str base
  * @returns {boolean}
  */
-function checkSpam(str) {
 
+function checkSpam(str) {
+  let lowerStr = str.toLowerCase();
+
+  return lowerStr.includes('1xBet') || lowerStr.includes('XXX');
 }
+
+alert( checkSpam('1XbeT now') );
+alert( checkSpam('free xxxxx') );
+alert( checkSpam('innocent rabbit') );
