06ff7b4f428b9a9d0fbc453d58ff24d39edad27f
Ruslan <inzhener063@mail.ru>
2020-03-19 21:23:37 +0400

    reshenie 3.2 3.4

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -3,5 +3,13 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
+const inputData = "1, -5.8 или 10, хотя 34 + -5.3 и 73";
+
+function getMinMax(string) {
+	var num_arr = string.match(/[+-]?\d+(?:\.\d+)?/g).map(Number);
+  
+  return {
+  	min : Math.min.apply(null, num_arr),
+    max : Math.max.apply(null, num_arr)
+  };
 }
