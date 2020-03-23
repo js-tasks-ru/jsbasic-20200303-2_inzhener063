70db6c53fe6895985f882680fbe8ce85725d28c1
Ruslan <inzhener063@mail.ru>
2020-03-23 18:56:02 +0400

    reshenie 4.1 4.2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,8 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+for (var i = 0; i < table.rows.length; i++) {
+  var row = table.rows[i];
+  row.cells[i].style.backgroundColor = 'red';
+}
 }
